486e61cf870972e2a9db081575f032ae1a50306c
jolleyily <muhthoriq73@gmail.com>
2025-10-03 10:00:04 +0700

    feat: add cta to sign in

--- app/components/DashboardNavbar.vue
+++ app/components/DashboardNavbar.vue
@@ -1,8 +1,15 @@
+<script setup lang='ts'>
+const user = useSupabaseUser()
+</script>
+
 <template>
   <UDashboardNavbar
     class="sticky lg:absolute top-0 inset-x-0 border-b-0 z-10 bg-default/75 backdrop-blur lg:bg-transparent lg:backdrop-blur-none pointer-events-none"
     :ui="{ left: 'pointer-events-auto', right: 'pointer-events-auto' }"
   >
+    <template v-if="!user" #left>
+      <span class="text-sm italic">Sign in to start chatting</span>
+    </template>
     <template #right>
       <UColorModeButton />
 
